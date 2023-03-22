33c03d1bd1c0949fe90382179b4b8eb5c821eefc
Edson Osmar Vasquez Navia <edzon.vazquez@gmail.com>
2023-03-22 15:27:40 -0400

    fix server-config not found

--- client/src/index.ts
+++ client/src/index.ts
@@ -7,11 +7,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('./home/build'));
 
-const config: ServerConfig = {
-    port: process.env.SERVER_PORT || 3000,
-    mongoServerUrl: process.env.MONGODB_URL || 'mongodb://localhost:27017/'
-};
-
 const enableServerMessage = () => {
     console.log(`Server listening on http://localhost:${config.port}/apirest`);
 };
